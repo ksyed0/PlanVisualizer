ba800e07beba9b80356061346094c28fc1c7f45e
ksyed0 <ksyed0@users.noreply.github.com>
2026-03-10 12:42:15 +0000

    deploy: 8fed5c5875a8ce5ab3f8e34d39d934d51c279d63

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/unit/**/*.test.js'],
-  collectCoverageFrom: ['tools/lib/**/*.js'],
-  coverageReporters: ['text', 'lcov', 'json-summary'],
-};
